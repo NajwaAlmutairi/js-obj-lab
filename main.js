fc427b40f7a9fb9016a43aba14d72783b92df1f7
najwa <NajwaBuayjan@gmail.com>
2024-09-10 11:44:41 +0300

    soliving lab 2

--- main.js
+++ main.js
@@ -47,3 +47,109 @@ console.log(user.skills[2]);
 // print the array skills  index with out the “ [] “ and Divide them with “,”
 console.log(user.skills);
 console.log(user.skills.join(','));
+
+
+// *************************************LAB2************************************************************
+console.log("**********lab 2 *********");
+
+
+characters = [
+    {
+        name: "Luke Skywalker",
+        height: 177,
+        gender: "male",
+        mass: 77,
+        eye_color: "brown",
+    },
+    {
+        name: "Leia Organa",
+        height: 160,
+        gender: "female",
+        mass: 56,
+        eye_color: "blue",
+    },
+    {
+        name: "Han Solo",
+        height: 180,
+        gender: "male",
+        mass: 80,
+        eye_color: "brown",
+    },
+    {
+        name: "Chewie",
+        height: 222,
+        gender: "male",
+        mass: 190,
+        eye_color: "black",
+    },
+    {
+        name: "kevien",
+        height: 106,
+        gender: "male",
+        mass: 32.2,
+        eye_color: "red",
+    },
+];
+
+//   1-Get first name that has blue eyes.
+
+let blue = characters.find(element => {
+    return element.eye_color === 'blue'
+})
+console.log(blue.name);
+
+// 2-Get first gender that is his mass is over 50
+let gender = characters.find(element => {
+    return element.mass > 50
+})
+console.log(gender.gender);
+
+// 3-Get characters with height less than 200
+let height = characters.filter(element => {
+    return element.height < 200
+})
+console.log(height);
+
+// 4-Get all male characters
+let maleCharacters = characters.filter(element => {
+    return element.gender ==='male'
+})
+console.log(maleCharacters);
+
+// 5-Get array of all names only
+let allNamesOnly = characters.map(element => {
+    return element.name
+})
+console.log(allNamesOnly);
+
+// 6-Get array of all heights only
+let allheightsOnly = characters.map(element => {
+    return element.height
+})
+console.log(allheightsOnly);
+
+//7-Sort by mass (low to high)
+const sortMass = characters.sort((a, b) => a.mass - b.mass);
+console.log(sortMass);
+
+console.log(',,,,,,,,,,');
+
+// 8-Sort by height (high to low)
+const sortHeight = characters.sort((a, b) => b.height - a.height);
+console.log(sortHeight); 
+
+// 9-Does every character have mass more than 40?
+let allMoreThan40 = characters.every(element => element.mass > 40)
+console.log('Does every character have mass more than 40?', allMoreThan40);
+
+// 10-Is every character shorter than 200?
+let allShorterThan = characters.every(element => element.height < 200)
+console.log('Is every character shorter than 200?', allShorterThan);
+
+// 11-Is there at least one character with blue eyes?
+let blueEyes = characters.some(element => element.eye_color === "blue")
+console.log('Is there at least one character with blue eyes', blueEyes);
+
+// 12-Is there at least one character taller than 210?
+let tallerThan210 = characters.some(element => element.height > 210)
+console.log('Is there at least one character taller than 210?', tallerThan210);
